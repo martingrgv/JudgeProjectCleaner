5cdc696445b3c28ad9d30195ec677fcce4b3de2f
martingrgv <martin.geor04@gmail.com>
2024-07-04 13:27:12 +0300

    Add project files.

---
+++ JudgeProjectCleaner/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace JudgeProjectCleaner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app removes obj and bin folders of your solution for judge submits.");
+            Console.WriteLine("Paste full path where .sln file is located:");
+
+            string? path = Console.ReadLine();
+
+            try
+            {
+                if (path != string.Empty && 
+                    path != null)
+                {
+                    ProjectCleaner.CleanSolution(path);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Empty path!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ JudgeProjectCleaner/ProjectCleaner.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Security;
+
+namespace JudgeProjectCleaner
+{
+    public class ProjectCleaner
+    {
+        private static readonly string[] removableDirectories = { "bin", "obj" };
+
+        public static void CleanSolution(string path)
+        {
+            if (isPathValid(path))
+            {
+                for (int i = 0; i < removableDirectories.Length; i++)
+                {
+                    var directories = Directory.GetDirectories(path, removableDirectories[i], SearchOption.AllDirectories);
+                    RemoveDirectories(directories);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Successfully deleted all unnecessary directories.");
+            }    
+            else
+            {
+                throw new InvalidOperationException($"Unable to find solution in path: {path}");
+            }
+        }
+
+        private static bool isPathValid(string path)
+        {
+            var files = Directory.GetFiles(path);
+
+            if (files.Length == 0)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Contains(".sln"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void RemoveDirectories(string[] directories)
+        {
+            foreach (var directory in directories)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(directory);
+                dirInfo.Delete(true);
+            }
+        }
+    }
+}
