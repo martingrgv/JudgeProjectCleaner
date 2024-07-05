3c739b300ed822f04fae9c047a3c8231d85927d1
martingrgv <martin.geor04@gmail.com>
2024-07-05 21:24:20 +0300

    Created loop and access to exit on escape press

--- JudgeProjectCleaner/Startup.cs
+++ JudgeProjectCleaner/Startup.cs
@@ -6,28 +6,43 @@ namespace JudgeProjectCleaner
     {
         static void Main(string[] args)
         {
+            Run();
+        }
+
+        private static void Run()
+        {
             Console.WriteLine("This app removes obj and bin folders of your solution for judge submits.");
-            Console.WriteLine("Paste full path where .sln file is located:");
 
-            string? path = Console.ReadLine();
-
-            Console.WriteLine();
-
-            try
+            while (true)
             {
-                if (path != string.Empty && 
-                    path != null)
+                Console.WriteLine("Paste full path where .sln file is located:");
+                string? path = Console.ReadLine();
+
+                Console.WriteLine(); 
+                try
                 {
-                    Console.WriteLine(FolderManagerService.RemoveUnnecesseryDirectories(path).Trim());
+                    if (path != string.Empty && 
+                        path != null)
+                    {
+                        Console.WriteLine(FolderManagerService.RemoveUnnecesseryDirectories(path).Trim());
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Empty path!");
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    throw new ArgumentException("Empty path!");
+                    Console.WriteLine(e.Message);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
+
+                Console.Write("Press any key to continue. If you want to exit press ESC: ");
+                if (Console.ReadKey().Key == ConsoleKey.Escape )
+                {
+                    return;
+                }
+
+                Console.WriteLine();
             }
         }
     }
